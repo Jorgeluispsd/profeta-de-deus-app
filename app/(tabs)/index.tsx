7d31b519a67b589739e20ae38d2268f619bc9a53
Jorgeluispsd <jorge.luisps09@gmail.com>
2025-04-23 21:10:12 -0300

    feat: setup project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,119 @@
+import { Image, Platform, StyleSheet, } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import MapView from 'react-native-maps';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#FFFFFF', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/pombo.jpg')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Igreja Profetas de Deus</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 1: Tente isto</ThemedText>
+        <ThemedText>
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '}
+          <ThemedText type="defaultSemiBold">
+            {Platform.select({
+              ios: 'cmd + d',
+              android: 'cmd + m',
+              web: 'F12'
+            })}
+          </ThemedText>{' '}
+          to open developer tools bora testar.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Onde nos encontramos:</ThemedText>
+        <ThemedText>
+          Nos encontramos na Rua Marli, 191 - Recanto Silvestre (Fazendinha) - São Paulo - SP.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Proximos Eventos: </ThemedText>
+        <ThemedText>
+          Os proximos eventos a acontecer vão ser:
+          {"\n"}
+          <ThemedText type="defaultSemiBold">Santa Ceia: </ThemedText>
+          Todo primeiro sabado do mês, as 19:00.
+          {"\n"}
+          <ThemedText type="defaultSemiBold">Culto dos jovens: </ThemedText>
+          Dia 10 de Maio de 2025, as 19:00.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -23.412778,
+  longitude: -46.884167,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01
+            }}
+          />
+        </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 4: Teste</ThemedText>
+        <ThemedText>
+          Um simples teste de uso
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 5: Teste1</ThemedText>
+        <ThemedText>
+          Um simples teste de uso do tamanho do texto
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 6: Mudança3</ThemedText>
+        <ThemedText>
+          Um simples teste de uso do tamanho da tela
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+  map: {
+    width: '100%',
+    height: '50%',
+  },
+});
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+
+export default function SettingsScreen() {
+    const [isEnabled, setIsEnabled] = React.useState(false);
+
+    const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Configurações</Text>
+        <View style={styles.settingItem}>
+          <Text style={styles.settingText}>Modo Noturno</Text>
+          <Switch
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
+        <View style={styles.settingItem}>
+          <Text style={styles.settingText}>Notificações</Text>
+          <Switch
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: '#fff',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 16,
+    },
+    settingItem: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: 16,
+    },
+    settingText: {
+      fontSize: 18,
+    },
+  });
